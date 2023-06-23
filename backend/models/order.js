0b41e3d36462c03e81d32cceb911cf4faa437ec8
daniel-jebarson <danieljebarson21@gmail.com>
2023-06-23 14:58:00 +0530

    🎨 Add : Schema for Order

---
+++ backend/models/order.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+    },
+    fromDate: {
+      type: String,
+      trim: true,
+    },
+    toDate: {
+      type: String,
+      trim: true,
+    },
+    K: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    KAC: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    D: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    DAC: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    S: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    SAC: {
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Orders = mongoose.models.Orders || mongoose.model("Order", orderSchema);
+
+module.exports = Orders;
