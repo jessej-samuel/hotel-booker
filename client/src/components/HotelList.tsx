b630ba0caed29db3e1d3ef868663d6660547ed2e
jessej-samuel <1234jessej@gmail.com>
2023-06-23 15:29:13 +0530

    Hotel List done

--- client/src/components/HotelList.tsx
+++ client/src/components/HotelList.tsx
@@ -1,34 +1,62 @@
+import { useEffect, useState } from "react";
+import ServerAPI from "../api/ServerAPI";
+import { useNavigate } from "react-router-dom";
+
 interface HotelType {
   name: string;
   location: string;
   imageLink: string;
-  _id?: string;
+  id: string;
 }
 
-type HotelListProps = {
-  hotels: HotelType[];
-};
-const HotelList: React.FC<HotelListProps> = ({ hotels = [] }) => {
-  return hotels.map((hotel: HotelType) => (
-    <HotelCard
-      name={hotel.name}
-      location={hotel.location}
-      imageLink={hotel.imageLink}
-      key={hotel._id}
-    />
-  ));
+const HotelList: React.FC = () => {
+  const [hotels, setHotels] = useState<HotelType[]>([]);
+  useEffect(() => {
+    ServerAPI.get("/hotel").then((res) => setHotels(res.data));
+  }, []);
+
+  return (
+    <div className="flex">
+      {hotels.map((hotel: HotelType) => (
+        <HotelCard
+          name={hotel.name}
+          location={hotel.location}
+          imageLink={
+            hotel.imageLink ||
+            "https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI="
+          }
+          key={hotel.id}
+          id={hotel.id}
+        />
+      ))}
+    </div>
+  );
 };
 
-const HotelCard = ({ name, location, imageLink }: HotelType) => {
+const HotelCard = ({ name, location, imageLink, id }: HotelType) => {
+  const navigate = useNavigate();
+
+  const handleBookClick = () => {
+    navigate("/hotels/" + id);
+  };
   return (
-    <div className="max-w-xs p-1 hover:bg-gray-100 transition-all cursor-default">
-      <img src={imageLink} alt="Hotel Image" className="rounded" />
+    <div className="max-w-xs p-1 transition-all cursor-default">
+      <img
+        src={imageLink}
+        alt="Hotel Image"
+        className="rounded saturate-0 hover:saturate-100 transition-all duration-500 group-hover:scale-105 hover:shadow"
+      />
       <div className="flex justify-between items-center">
         <div>
-          <p className="mt-2 uppercase">{name}</p>
-          <p className="text-gray-500 text-sm font-medium">{location}</p>
+          <p className="mt-2 text-lg ">{name}</p>
+          <p className="text-gray-500 text-xs font-medium uppercase">
+            {location}
+          </p>
         </div>
-        <button className="bg-blue-600 px-2 py-2 text-white hover:bg-blue-500 rounded">
+        <button
+          className="bg-blue-600 px-2 py-2 text-white hover:bg-blue-500 rounded hover:shadow"
+          onClick={handleBookClick}
+        >
           Book Now
         </button>
       </div>
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,16 +4,7 @@ const Dashboard = () => {
   return (
     <div className="mt-8 px-6">
       <h1 className="text-4xl font-bold mb-4">Hello!</h1>
-      <HotelList
-        hotels={[
-          {
-            name: "Hotel 1",
-            location: "Location 1",
-            imageLink:
-              "https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=",
-          },
-        ]}
-      />
+      <HotelList />
     </div>
   );
 };
