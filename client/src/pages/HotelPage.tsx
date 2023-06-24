7577e02d252c769e4aa1f041d5a2e9625612a76e
jessej-samuel <1234jessej@gmail.com>
2023-06-24 16:10:44 +0530

    🔥 feat: reactive total cost

--- client/src/pages/HotelPage.tsx
+++ client/src/pages/HotelPage.tsx
@@ -1,11 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import ServerAPI from "../api/ServerAPI";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+type HotelMetaData = {
+  _id: string;
+  name: string;
+  location: string;
+  email: string;
+  password: string;
+  K: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  KAC: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  D: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  DAC: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  S: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  SAC: {
+    count: number;
+    cost: number;
+    _id: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
 const HotelPage = () => {
   const { id } = useParams();
+  const [total, setTotal] = useState(0);
+  const [roomType, setRoomType] = useState(
+    "K" as "K" | "KAC" | "D" | "DAC" | "S" | "SAC"
+  );
+  const [from, setFrom] = useState(new Date().toISOString().slice(0, 10));
+  const [to, setTo] = useState(new Date().toISOString().slice(0, 10));
   const [hotelMetaData, setHotelMetaData] = useState({
     _id: "64941e6f5077923fadc8beb4",
     name: "Dani",
@@ -45,13 +92,52 @@ const HotelPage = () => {
     createdAt: "2023-06-22T10:11:59.417Z",
     updatedAt: "2023-06-22T10:11:59.417Z",
     __v: 0,
-  });
+  } as HotelMetaData);
+
+  const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
     ServerAPI.get(`/hotel/${id}`).then((response) => {
       setHotelMetaData(response.data);
     });
   }, [id]);
+
+  useEffect(() => {
+    if (from && to) {
+      const fromTime = new Date(from).getTime();
+      const toTime = new Date(to).getTime();
+      const diff = toTime - fromTime;
+      const days = diff / (1000 * 60 * 60 * 24);
+      setTotal(days * hotelMetaData[roomType].cost);
+      console.log(total);
+    }
+  }, [from, to, roomType, total]);
+
+  const handleFromChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFrom(e.target.value);
+  };
+
+  const handleToChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value < from) {
+      alert("Please select a date after the start date");
+      return;
+    }
+    setTo(e.target.value);
+  };
+
+  const handleRoomTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRoomType(e.target.value as any);
+  };
+
+  const handleBookNowClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log({
+      hotelId: id,
+      roomType,
+      from,
+      to,
+    });
+  };
 
   return (
     <div className="mx-auto my-5 px-8 max-w-4xl">
@@ -91,6 +177,65 @@ const HotelPage = () => {
         combines warm hospitality with a lovely ambiance to make your stay in
         Atlantis unforgettable.
       </p>
+      <h1 className="text-2xl font-bold my-4">Book a Room</h1>
+      <div className="flex justify-between items-end">
+        <form className="w-96" ref={formRef}>
+          <div className="flex flex-col mt-4">
+            <label className="text-sm mb-2 text-gray-600 uppercase font-medium">
+              Check In
+            </label>
+            <input
+              type="date"
+              defaultValue={from}
+              onChange={handleFromChange}
+              className="border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div className="flex flex-col mt-4">
+            <label className="text-sm mb-2 text-gray-600 uppercase font-medium">
+              Check Out
+            </label>
+            <input
+              type="date"
+              defaultValue={to}
+              onChange={handleToChange}
+              className="border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div className="flex flex-col mt-4">
+            <label className="text-sm mb-2 text-gray-600 uppercase font-medium">
+              Room Type
+            </label>
+            <select
+              className="border border-gray-300 rounded-md p-2"
+              onChange={handleRoomTypeChange}
+            >
+              <option value="K">King (${hotelMetaData.K.cost})</option>
+              <option value="KAC">
+                {" "}
+                King with AC (${hotelMetaData.KAC.cost})
+              </option>
+              <option value="D">Double (${hotelMetaData.D.cost})</option>
+              <option value="DAC">
+                Double with AC (${hotelMetaData.DAC.cost})
+              </option>
+              <option value="S">Suite (${hotelMetaData.S.cost})</option>
+              <option value="SAC">
+                Suite with AC (${hotelMetaData.SAC.cost})
+              </option>
+            </select>
+          </div>
+        </form>
+        <div className="flex items-baseline gap-x-4">
+          <p className="font-semibold text-2xl text-right">${total}</p>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+            onClick={handleBookNowClick}
+          >
+            Book Now
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
