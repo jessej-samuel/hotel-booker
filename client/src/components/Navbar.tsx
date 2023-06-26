2ef32cc4c1f3533434315302d64c27850485d93f
jessej-samuel <1234jessej@gmail.com>
2023-06-26 15:40:41 +0530

    navbar login/logout states

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,62 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../utils/hooks";
 
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
+  const navigate = useNavigate();
+  useAuth();
+  const logout = () => {
+    localStorage.removeItem("userData");
+    navigate("/");
+  };
+
+  const renderNavLinks = () => {
+    const userData = JSON.parse(localStorage.getItem("userData") || "{}");
+
+    if (localStorage.getItem("userData"))
+      return (
+        <div
+          onMouseLeave={() => {
+            if (showLogout) setShowLogout(false);
+          }}
+        >
+          <div
+            className="flex items-center gap-x-3 cursor-pointer "
+            onClick={() => setShowLogout((s) => !s)}
+          >
+            <p onSelect={(e) => e.preventDefault()}>{userData.name}</p>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA7UbmM2x9QWw6Sga9Cc4x2INxD5MwiyhvxjWNKjuyxz3gAwcjM1kLab-vRfsmypHx1JE&usqp=CAU"
+              className="aspect-square w-10 rounded-full"
+            />
+          </div>
+          <div
+            className="absolute bg-white right-0 px-4 py-2 rounded-md border text-red-500 hover:bg-red-500 hover:text-white hover cursor-pointer z-10 transition-all"
+            hidden={!showLogout}
+            onClick={logout}
+          >
+            Logout
+          </div>
+        </div>
+      );
+    return (
+      <div className="pr-8 md:block">
+        <a
+          href="/hotel/login"
+          className="p-4 hover:text-blue-500 transition duration-300 ease-in"
+        >
+          Hotel Login
+        </a>
+        <a
+          href="/user/login"
+          className="p-4 hover:text-blue-500 transition duration-300 ease-in"
+        >
+          User Login
+        </a>
+      </div>
+    );
+  };
   return (
     <nav className="flex justify-between items-center h-16 bg-white text-black relative border-b shadow-sm font-mono">
       <a href="/" className="pl-8">
@@ -23,20 +79,7 @@ const Navbar = () => {
           ></path>
         </svg>
       </div>
-      <div className="pr-8 md:block">
-        <a
-          href="/user/register"
-          className="p-4 hover:text-gray-500 transition duration-300 ease-in"
-        >
-          Register
-        </a>
-        <a
-          href="/user/login"
-          className="p-4 hover:text-gray-500 transition duration-300 ease-in"
-        >
-          Login
-        </a>
-      </div>
+      {renderNavLinks()}
     </nav>
   );
 };
