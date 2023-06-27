fda3ae8de91dc7176f02da0b7595e7c67d5cd698
daniel-jebarson <danieljebarson21@gmail.com>
2023-06-27 16:42:26 +0530

    🚑 Order : route

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const { CustomError } = require("../error/custom");
 const HotelModel = require("../models/hotel");
 const OrderModel = require("../models/order");
+const Axios = require("axios");
 
 const orderHotel = asyncHandler(async (req, res) => {
   try {
@@ -13,15 +14,33 @@ const orderHotel = asyncHandler(async (req, res) => {
     if (!userId || !userName || !hotelId || !fromDate || !toDate) {
       throw new CustomError("Please specify the required fields", 400);
     }
+
     try {
       const HotelExists = await HotelModel.findOne({
         _id: hotelId,
       });
-      console.log(HotelExists);
+      // console.log(HotelExists);
     } catch (error) {
+      // console.log(error);
       throw new CustomError("No such hotel exists!", 400);
     }
-
+    const { data } = await Axios.get(
+      `http://localhost:5000/hotel/${hotelId}/availability?from=${fromDate}&to=${toDate}`
+    );
+    console.log(data);
+    if (
+      (K && K.count > data.K) ||
+      (KAC && KAC.count > data.KAC) ||
+      (D && D.count > data.D) ||
+      (DAC && DAC.count > data.DAC) ||
+      (S && S.count > data.S) ||
+      (SAC && SAC.count > data.SAC)
+    ) {
+      throw new CustomError(
+        "Sorry we don't have that much rooms free right now",
+        400
+      );
+    }
     const Order = await OrderModel.create({
       userId,
       hotelId,
