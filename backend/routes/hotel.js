9cf43bb62b4b8e3ff79ee3a020f27f153ee5659b
daniel-jebarson <danieljebarson21@gmail.com>
2023-06-28 15:27:52 +0530

    ✅ Add : Hotel Orders route

--- backend/routes/hotel.js
+++ backend/routes/hotel.js
@@ -6,11 +6,13 @@ const {
   getHotelById,
   getHotelBookings,
   isBookAllowed,
+  getHotelOrders,
 } = require("../controllers/hotel");
 
 router.route("").get(getAllHotels);
 router.route("/:hotelId/stats").get(getHotelBookings);
 router.route("/:hotelId/availability").get(isBookAllowed);
+router.route("/:hotelId/orders").get(getHotelOrders);
 router.route("/:hotelId").get(getHotelById);
 
 module.exports = router;
