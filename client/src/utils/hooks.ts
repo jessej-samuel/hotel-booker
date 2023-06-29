6182ab6dd4c1b4da60c4f0733ec369e41af8a4ad
jessej-samuel <1234jessej@gmail.com>
2023-06-29 16:59:42 +0530

    feat: order history

--- client/src/utils/hooks.ts
+++ client/src/utils/hooks.ts
@@ -4,8 +4,9 @@ import { useNavigate } from "react-router-dom";
 const useAuth = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({
-    _id: "juj",
+    _id: "",
     name: "",
+    type: "",
   });
 
   useEffect(() => {
@@ -13,14 +14,12 @@ const useAuth = () => {
       JSON.parse(localStorage.getItem("userData") || "{}") || undefined;
     const path = window.location.pathname;
 
-    console.log({ userData, path });
-
     if (path === "/hotel/login" && userData.type === "hotel")
       navigate("/dashboard");
     else if (path === "/user/login" && userData.type === "user")
       navigate("/dashboard");
     if (!userData || userData === undefined) navigate("/");
-    else if (path === "/" && userData.type) navigate("/dashboard");
+    else if (path === "/" && userData.type != "") navigate("/dashboard");
     else setUserData(userData);
   }, [navigate]);
 
